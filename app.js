2ea2b9829834009416022c124ad29df7b4b8c223
deysagar523 <deysagar523@gmail.com>
2022-03-21 14:50:12 +0530

    change callback url

--- app.js
+++ app.js
@@ -303,7 +303,7 @@
   passport.use(new GoogleStrategy({
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/secrets",
+      callbackURL: "https://blooming-reaches-75363.herokuapp.com/auth/google/secrets",
       userProfileUrl: "https://www.googleapis.com/oauth2/v3/userinfo"
     },
     function(req, accessToken, refreshToken, profile, cb) {
